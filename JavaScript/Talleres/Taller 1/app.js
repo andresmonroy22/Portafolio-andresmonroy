3c9b7a2ca681441b2442a12025c4afd213d6c419
andresmonroy22 <145635288+andresmonroy22@users.noreply.github.com>
2023-10-09 19:38:59 -0500

    se realizan cambios

--- JavaScript/Talleres/Taller 1/app.js
+++ JavaScript/Talleres/Taller 1/app.js
@@ -1,3 +1,5 @@
 //Declaracion de variables
 
-let Nombre,Edad
+let Nombre = promnt("ingrese el nombre : ");
+let edad = promnt("ingrese su edad : ");
+alert(`Bienvenido, ${Nombre} veo que tienes ${Edad}`);
