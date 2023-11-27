631235d2708c71620e83cb85afeeccd394a81443
andresmonroy22 <145635288+andresmonroy22@users.noreply.github.com>
2023-11-27 16:04:47 -0500

    Actualizaciones para la segunda parte del trabajo

--- JavaScript/Talleres/Taller 1/app.js
+++
@@ -1,7 +0,0 @@
-//Declaracion de variables
-
-let name = promnt("ingrese el nombre : ");
-let age = promnt("ingrese su edad : ");
-alert(`Bienvenido, ${Nombre} veo que tienes ${Edad}`);
-console.log(age),
-console.log(name)
--- segundoproyecto/app.js
+++ segundoproyecto/app.js
@@ -3,15 +3,29 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const { create } = require('express-handlebars')
+// Modulo para dar formato a las fechas
+const moment = require('moment')
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
+
+//Configuración de express con handlebars para crear los helpers
+const hbs = create({
+  // Establecemos la extension de handlebars para los archivos que es .hbs
+  extname: '.hbs'
+})
+
+// Creación del helper que sera una funcion para formatear fechas
+hbs.handlebars.registerHelper('formatoFecha', function (fecha){
+  return moment(fecha).format('YYYY-MM-DD')
+})
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+app.engine('.hbs', hbs.engine)
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -20,7 +34,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
